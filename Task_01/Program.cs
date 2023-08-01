9d74f1c3cbff5308d0023c6e37ad3d65b6cc2ea4
ZordonLord <zordonlord@mail.ru>
2023-08-01 23:02:12 +1000

    add seminar's tasks

---
+++ Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Введите число 1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2*number2)
+
+{
+Console.WriteLine("да:1ое число является квадратом 2го числа");
+}
+else
+{
+ Console.WriteLine("нет:1ое число не является квадратом 2го числа");   
+}
---
+++ Task_03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+Console.Write("Введите номер дня недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+string dayName = getWeekName(dayNum);
+
+Console.WriteLine($"{dayNum} день недели называется '{dayName}'");
+
+
+
+string getWeekName(int dayNum)
+{
+
+    switch (dayNum)
+    {
+        case 1: return "Понедельник";
+        case 2: return "Вторник";
+        case 3: return "Среда";
+        case 4: return "Четверг";
+        case 5: return "Пятница";
+        case 6: return "Суббота";
+        case 7: return "Воскресенье";
+
+        default:
+            return "Неизвестный день недели";
+    }
+}
