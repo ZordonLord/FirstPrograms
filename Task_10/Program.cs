484c022ff106f385d5aaa859f6e68439d1693451
ZordonLord <zordonlord@mail.ru>
2023-08-07 23:28:58 +1000

    добавили задачи 10,13,15

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (99 < number && number < 1000)
+{
+    int res = (number % 100) / 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string dayoff = Dayoff(number);
+Console.WriteLine(dayoff);
+
+string Dayoff(int numb)
+{
+    if (numb > 0 && numb <= 7)
+    {
+        return numb == 6 || numb == 7 ? "Да" : "Нет";
+    }
+    return "Некорректный день недели";
+}
