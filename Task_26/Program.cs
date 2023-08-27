9da053746d8f96fabd7e27568e48954478a5663b
ZordonLord <zordonlord@mail.ru>
2023-08-27 22:32:55 +1000

    добавлены задачи 4 семинара

---
+++ Task_26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Quantity(int num)
+{
+    // if (num < 0) num = num * -1;
+    if (num == 0) return 1;
+    int count = 0;
+    while (num != 0)
+    {
+        num = num /10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantity = Quantity(number);
+Console.WriteLine($"В числе {number} -> {quantity}");
