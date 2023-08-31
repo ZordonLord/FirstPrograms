9271fe5c9242f36d264728ef33fa6c038729c983
ZordonLord <zordonlord@mail.ru>
2023-08-31 23:19:20 +1000

    добавлены задачи 5 семинара

---
+++ Task_35/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountTwoDigitNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(123, -100, 100);
+PrintArray(array);
+int countTwoDigitNumbers = CountTwoDigitNumbers(array);
+Console.WriteLine($"Количество элементов в диапазоне [10,99]: {countTwoDigitNumbers}");
