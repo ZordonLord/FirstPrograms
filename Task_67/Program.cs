bcbdb61a6b69e29e50fe4b19f24bf7ee1b401f5d
ZordonLord <zordonlord@mail.ru>
2023-09-12 21:50:57 +1000

    Добавлены задачи семинара 9

---
+++ Task_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigitNum(int num)
+ {
+    if (num == 0) return 0;
+    return num % 10 + SumDigitNum(num / 10); 
+ }
+
+ Console.WriteLine("Введите число:");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine(SumDigitNum(number));
